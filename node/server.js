1837a7570e1997669658dddc3d4f76d235b319a2
neiheivj <neiheivj@mail.uc.edu>
2019-10-01 22:55:46 -0400

    Updated server.js

--- node/server.js
+++ node/server.js
@@ -2,6 +2,7 @@ var http = require("http");
 var fs = require("fs");
 var os = require("os");
 var ip = require("ip");
+var math = require("math");
 
 http.createServer(function(req, res){
     if (req.url === "/"){
@@ -11,11 +12,15 @@ http.createServer(function(req, res){
         });
     }
     else if (req.url.match("/sysinfo")){
-       myHostName=os.hostname();
-       myServerUptime = 0;
-       myTotalMemory = 0;
-       myFreeMemory = 0;
-       myCPUCount = 0;
+        myHostName=os.hostname();
+        myServerUptime = os.uptime();
+        myDay=Math.floor(myServerUptime / 86400);
+        myHours=Math.floor((myServerUptime - (myDay * 86400)) / 3600);
+        myMinutes=Math.floor((myServerUptime - (myDay * 86400) - (myHours * 3600)) / 60);
+        mySeconds=Math.floor(myServerUptime - (myDay * 86400) - (myHours * 3600) - (myMinutes * 60));
+        myTotalMemory = os.totalmem();
+        myFreeMemory = os.freemem();
+        myCPUCount = os.cpus().length;
         html=`<!DOCTYPE HTML>
             <HTML>
                 <HEAD>
@@ -24,9 +29,9 @@ http.createServer(function(req, res){
                 <body>
                     <p> Hostname: ${myHostName} </p>
                     <p> IP: ${ip.address()} </p>
-                    <p> Server Uptime: ${myServerUptime}</p>
-                    <p> Total Memory: ${myTotalMemory}</p>
-                    <p> Free Memory: ${myFreeMemory}</p>
+                    <p> Server Uptime: Days: ${myDay}, Hours: ${myHours}, Minutes: ${myMinutes}, Seconds: ${mySeconds}</p>
+                    <p> Total Memory: ${Math.floor(myTotalMemory / 1048576)} MB</p>
+                    <p> Free Memory: ${Math.floor(myFreeMemory / 1048576)} MB</p>
                     <p> Number of CPUs: ${myCPUCount}</p>
                 </body>
             </HTML>
