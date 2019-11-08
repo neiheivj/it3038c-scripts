8969e1a245414967acdaad9a34cdeecfe92e93be
neiheivj <neiheivj@mail.uc.edu>
2019-11-07 23:50:42 -0500

    Lab9

--- Python/weatherapi.py
+++ Python/weatherapi.py
@@ -1,6 +1,6 @@
 import json
 import requests
 zip = input()
-r = requests.get('http://sample.openweathermap.org/data/2.5/weather?zip=%s,us&appid=b1b15e88fa797225412429c1c50c122a1' % zip)
+r = requests.get('http://samples.openweathermap.org/data/2.5/weather?zip=%s,us&appid=b6907d289e10d714a6e88b30761fae22' % zip)
 data=r.json()
 print(data['weather'][0]['description'])
